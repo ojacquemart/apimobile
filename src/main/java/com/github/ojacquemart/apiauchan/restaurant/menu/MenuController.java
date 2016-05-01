302431b2e26354c64c09818dd8943376c3c47cdc
Olivier Jacquemart <o.jacquemart@gmail.com>
2016-05-01 22:52:31 +0200

    chore: remove slack client & scheduled task

--- src/main/java/com/github/ojacquemart/apiauchan/restaurant/menu/MenuController.java
+++ src/main/java/com/github/ojacquemart/apiauchan/restaurant/menu/MenuController.java
@@ -1,34 +1,16 @@
 package com.github.ojacquemart.apiauchan.restaurant.menu;
 
-import com.github.ojacquemart.apiauchan.restaurant.menu.slack.MessageSender;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor(onConstructor = @__(@Autowired))
 @RequestMapping("/apiauchan")
 @RestController
 public class MenuController {
 
-    private MessageSender sender;
-
-    @RequestMapping(value = "/menus", method = RequestMethod.POST)
-    @Scheduled(cron = "0 0 10 * * 1-5")
-    public void postMenus() {
-        String menu = getMenuAsString();
-        sender.sendMenu(menu);
-    }
-
     @RequestMapping(value = "/menus", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     public String getMenusAsString() {
-        return getMenuAsString();
-    }
-
-    private String getMenuAsString() {
         return MenuParser.parseFromUrl().toString();
     }
 
