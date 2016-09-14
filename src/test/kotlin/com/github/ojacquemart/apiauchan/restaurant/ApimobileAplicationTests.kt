11d5a4314fab51716a0350aca51fb70e9aec127a
ojacquemart <o.jacquemart@gmail.com>
2016-09-14 23:55:27 +0200

    chore: introduce kotlin configuration

---
+++ src/test/kotlin/com/github/ojacquemart/apiauchan/restaurant/ApimobileAplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.ojacquemart.apiauchan.restaurant
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ApimobileAplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
